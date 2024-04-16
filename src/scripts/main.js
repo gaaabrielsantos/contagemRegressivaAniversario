0785cfe651e2ae1e094bd4ddd71234cc4463f96c
gaaabrielsantos <grvs.93@gmail.com>
2024-04-16 07:11:57 -0300

    criando a contagem regressiva

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,27 @@
-console.log("olá mundo");
+AOS.init();
+
+const dataDoEvento = new Date("May 12, 2024 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval(function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diasEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diasEmMs);
+    const horasRestantes = Math.floor((distanciaAteOEvento % diasEmMs) / horasEmMs);
+    const minutosRestantes = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+    const segundosRestantes = Math.floor((distanciaAteOEvento % minutosEmMs) / 1000);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasRestantes}h ${minutosRestantes}m ${segundosRestantes}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(horasRestantes);
+        document.getElementById('contador').innerHTML = 'Evento expirado';
+    }
+}, 1000);
